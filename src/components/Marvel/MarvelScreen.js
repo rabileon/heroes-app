bb26dbdbbfe2fba243b50937ab3efc57c762fcf2
Rabi León <rabi_leon@hotmail.com>
2022-03-12 08:21:49 -0600

    🚧 lista heroes

--- src/components/Marvel/MarvelScreen.js
+++ src/components/Marvel/MarvelScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { HeroList } from '../hero/HeroList';
 
 export const MarvelScreen = () => {
-  return <div>MarvelScreen</div>;
+  const publisher = 'Marvel Comics';
+  return (
+    <>
+      <h1>MarvelScreen</h1>
+      <hr />
+      <HeroList publisher={publisher}></HeroList>
+    </>
+  );
 };
--- src/components/dc/DcScreen.js
+++ src/components/dc/DcScreen.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { HeroList } from '../hero/HeroList';
 
 export const DcScreen = () => {
+  const publisher = 'DC Comics';
   return (
-    <div>
+    <>
       <h1>DCScreen</h1>
-    </div>
+      <hr />
+      <HeroList publisher={publisher} />
+    </>
   );
 };
