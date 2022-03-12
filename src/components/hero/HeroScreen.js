9b889cb566c122776f0904ba6ff1707a8e1e4ad9
Rabi León <rabi_leon@hotmail.com>
2022-03-12 12:32:29 -0600

    🚧 params navigate

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,7 +1,19 @@
+import { Navigate, useParams } from 'react-router-dom';
+import { getHeroById } from '../../selectors/getHeroById';
+
 export const HeroScreen = () => {
+  const { heroId } = useParams();
+
+  const hero = getHeroById(heroId);
+
+  if (!hero) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div>
       <h1>Hero Screen</h1>
+      <p>{hero.superhero}</p>
     </div>
   );
 };
