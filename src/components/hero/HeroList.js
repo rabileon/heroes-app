8307451f04a2f6e0acbbcb70516de5eff4c9e7d5
Rabi León <rabi_leon@hotmail.com>
2022-03-12 12:15:20 -0600

    🚧 hero card

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,14 +1,13 @@
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
   return (
-    <>
-      <ul>
-        {heroes.map((hero) => (
-          <li key={hero.id}>{hero.superhero}</li>
-        ))}
-      </ul>
-    </>
+    <div className="row rows-cols-1 row-cols-md-3 g-3">
+      {heroes.map((hero) => (
+        <HeroCard key={hero.id} {...hero} />
+      ))}
+    </div>
   );
 };
